2293d8f8aa5c78981f86157d0d9c430da58a3dc6
Dor <simhondor@gmail.com>
2017-12-25 13:23:28 +0200

    .

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ $('.post-band').click(function (name, genre, rating) {
     genre = $('.genre-input').val();
     rating = $('.band-rating').val();
     addBand(name, genre, rating);
-    renderBeers();
+    renderBands();
 })
 
-function renderBeers() {
+function renderBands() {
     $('.band-input').val('')
     $('.genre-input').val('')
     $('.bands-list').find('li').remove();
@@ -30,16 +30,18 @@ function renderBeers() {
 $('.reverse-order').click(function () {
     if (sorted === false) {
         bands.sort(function (a, b) {
+            $('.reverse-order').html("Sort by Ascending")
             if (a.bandRating < b.bandRating) return -1;
             if (a.bandRating > b.bandRating) return 1;
             return 0;
         })
         sorted = true;
     } else if (sorted === true){
+        $('.reverse-order').html("Sort by Descending")
         bands.reverse();
         sorted = false;
     }
-    renderBeers();
+    renderBands();
 })
 
 var bands = [];
